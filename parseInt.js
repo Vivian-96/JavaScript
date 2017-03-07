523614ecf82f1e610f1095b4fb93521d07bc0ae9
Vivian <1754495139@qq.com>
2017-03-07 15:00:22 +0800

    a JS file about parseInt

---
+++ parseInt.js
@@ -0,0 +1,24 @@
+function parseInt()
+{
+	var num1 = parseInt("1234blue");
+	alert(num1); 
+}
+
+function addLoadEvent(func)
+{
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function')
+	{
+		window.onload = func;
+	}
+	else
+	{
+		window.onload = function()
+		{
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(parseInt);
