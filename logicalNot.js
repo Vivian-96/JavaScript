f75f333124311745fc0b7aef0a22d241c7c1a3a2
Vivian <1754495139@qq.com>
2017-03-07 14:47:38 +0800

    a JS file about Not

---
+++ logicalNot.js
@@ -0,0 +1,28 @@
+function logicalNot()
+{
+	alert("!false = " + !false);
+	alert("!'blue' = " + !"blue");
+	alert("!0 = " + !0);
+	alert("!NaN = " + !NaN);
+	alert("!'' = " + !"");
+	alert("!12345 = " + !12345);
+}
+
+function addLoadEvent(func)
+{
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function')
+	{
+		window.onload = func;
+	}
+	else
+	{
+		window.onload = function()
+		{
+			oldonload();
+			func();
+		}
+	}
+}
+
+addLoadEvent(logicalNot);
